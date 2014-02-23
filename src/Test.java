273ab8bdfc322b9dabbbebf31a737ef479eb0b0b
stonemount1 <stonemount1@gmail.com>
2014-02-22 18:48:34 -0600

    testing commit

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,8 @@ import static java.lang.System.out;
 public class Test {
     public static void main(String[] args) {
         out.println("hello world");
+
+        int x = 5;
+        out.println("x: " + x);
     }
 }
