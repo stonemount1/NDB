15d033f34072d5b96d2c361a02cb7189709d8f29
stonemount1 <stonemount1@gmail.com>
2014-07-21 22:44:05 -0500

    Update Test.java
    Updating in GitHub

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@ import static java.lang.System.out;
 
 public class Test {
     public static void main(String[] args) {
-        out.println("hello world");
+        out.println("hello world and goodbye");
 
         int x = 5;
         out.println("x: " + x);
